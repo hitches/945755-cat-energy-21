1c3d58ae1a68b98da327328974ab9920f4968f53
Кирилл <kirill-vologda@yandex.ru>
2021-01-19 01:04:42 +0300

    Замечания по таблице

--- source/js/script.js
+++ source/js/script.js
@@ -4,11 +4,11 @@ var navToggle = navMain.querySelector('.site-nav__toggle');
 navMain.classList.remove('site-nav--nojs');
 
 navToggle.addEventListener('click', function () {
-  if (navMain.classList.contains('site-nav--closed')) {
-    navMain.classList.remove('site-nav--closed');
-    navMain.classList.toggle('site-nav--opened');
-  } else {
-    navMain.classList.add('site-nav--closed');
+  if (navMain.classList.contains('site-nav--opened')) {
     navMain.classList.remove('site-nav--opened');
+    navMain.classList.toggle('site-nav--closed');
+  } else {
+    navMain.classList.add('site-nav--opened');
+    navMain.classList.remove('site-nav--closed');
   }
 });
