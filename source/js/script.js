93049922d15d1ac7f35f94381ea883b2f6e990aa
Кирилл <kirill-vologda@yandex.ru>
2021-01-21 01:03:20 +0300

    мелкие фиксы

--- source/js/script.js
+++ source/js/script.js
@@ -3,12 +3,16 @@ var navToggle = navMain.querySelector('.site-nav__toggle');
 
 navMain.classList.remove('site-nav--nojs');
 
-navToggle.addEventListener('click', function () {
-  if (navMain.classList.contains('site-nav--opened')) {
-    navMain.classList.remove('site-nav--opened');
-    navMain.classList.toggle('site-nav--closed');
-  } else {
-    navMain.classList.add('site-nav--opened');
+navToggle.classList.add('site-nav--opened');
+navMain.classList.add('site-nav--closed');
+
+
+navToggle.addEventListener('click', function() {
+  if (navMain.classList.contains('site-nav--closed')) {
     navMain.classList.remove('site-nav--closed');
+    navMain.classList.add('site-nav--opened');
+  } else {
+    navMain.classList.add('site-nav--closed');
+    navMain.classList.remove('site-nav--opened');
   }
 });
