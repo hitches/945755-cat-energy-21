ad745a07da2fe8349d9cb77fefd07fc3c9a5ee0a
Кирилл <kirill-vologda@yandex.ru>
2021-01-22 02:06:57 +0300

    фиксики

--- source/js/script.js
+++ source/js/script.js
@@ -3,8 +3,10 @@ var navToggle = navMain.querySelector('.site-nav__toggle');
 
 navMain.classList.remove('site-nav--nojs');
 
-navToggle.classList.add('site-nav--opened');
+navToggle.classList.add('site-nav--closed');
+navToggle.classList.remove('site-nav--opened');
 navMain.classList.add('site-nav--closed');
+navMain.classList.remove('site-nav--opened');
 
 
 navToggle.addEventListener('click', function() {
